03287025273ba1779cfe04089bcfe3e15e844048
SrFranvs <1885453@alu.murciaeduca.es>
2024-02-07 01:12:58 +0100

    4.2 Modificaciones en la otra rama

--- Principal.java
+++ Principal.java
@@ -1,5 +1,12 @@
+import java.util.Date;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        mostrarFechaHoraActual();
+    }
+    public static void FechaHoraYa() {
+        Date fechaHoraYa = new Date();
+        System.out.println("Fecha y hora: " + fechaHoraActual);
     }
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Modificación
+
+Nuevo método en el archivo Principal.java1 que muestra la fecha y hora actual
+
+Quiero resaltar que antes de cambiarme a la rama en la que estamos trabajando se me ha olvidado crear el documento readme vacío, pero lo creo ahora con estos comentarios
